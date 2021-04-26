82c66ade5ec23b9f18a971053e5e62ff0ca71a36
Oren Henderson-Zintz <orenmhz@gmail.com>
2021-04-26 16:15:48 -0500

    Includes code for displaying a list in the CardSets tab and a floating action button.

--- flashCards/MainActivity.cs
+++ flashCards/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.BottomNavigation;
+using Google.Android.Material.FloatingActionButton;
 
 namespace flashCards
 {
@@ -12,6 +13,8 @@ namespace flashCards
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
         TextView textMessage;
+        ListView cardSetsListView;
+        FloatingActionButton newSetButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,6 +23,8 @@ namespace flashCards
             SetContentView(Resource.Layout.activity_main);
 
             textMessage = FindViewById<TextView>(Resource.Id.message);
+            cardSetsListView = FindViewById<ListView>(Resource.Id.listView1);
+            newSetButton = FindViewById<FloatingActionButton>(Resource.Id.fab);
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
         }
@@ -35,15 +40,38 @@ namespace flashCards
             {
                 case Resource.Id.navigation_home:
                     textMessage.SetText(Resource.String.title_home);
+                    cardSetsListView.Visibility = ViewStates.Gone;
+                    textMessage.Visibility = ViewStates.Visible;
                     return true;
                 case Resource.Id.cardSet_dashboard:
-                    textMessage.SetText(Resource.String.title_cardSet);
+                    ShowCardSets();
                     return true;
                 case Resource.Id.setting_dashboard:
                     textMessage.SetText(Resource.String.title_settings);
+                    cardSetsListView.Visibility = ViewStates.Gone;
+                    textMessage.Visibility = ViewStates.Visible;
                     return true;
             }
             return false;
+        }
+
+        public void ShowCardSets()
+        {
+            string[] entries = new string[] { "1", "2", "3" };      //will be replaced by code to load existing card sets
+            if(entries.Length == 0)
+            {
+                textMessage.SetText(Resource.String.title_cardSet);
+                textMessage.Visibility = ViewStates.Visible;
+            }
+            else
+            {
+                textMessage.Visibility = ViewStates.Gone;
+                cardSetsListView.Visibility = ViewStates.Visible;
+                ArrayAdapter adapter = new ArrayAdapter<string>(this, Resource.Layout.activity_listview, entries);
+                cardSetsListView.Adapter = adapter;
+
+                //TODO add onClickListener
+            }
         }
     }
 }
